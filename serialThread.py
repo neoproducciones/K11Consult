7a1e9132d09395df7d37db28b28004679cc5e293
Javier <neoproducciones@gmail.com>
2016-09-13 23:40:38 +0200

    Tidying up

--- serialThread.py
+++ serialThread.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # serialThread.py
 
-#Copyright (C) 2014 Eilidh Fridlington http://eilidh.fridlington.com
+# Copyright (C) 2014 Eilidh Fridlington http://eilidh.fridlington.com
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 
 import time
@@ -23,33 +23,29 @@ import threading
 import datetime
 
 
-
 class ReadStream(threading.Thread):
-
-    def __init__(self, port,connected = False):
+    def __init__(self, port, connected=False):
         self.port = port
-        self.connected  = connected
+        self.connected = connected
         self.stream = False
         self.integrity = False
 
-        # D es una variable global de tipo diccionario
-
         self.D = dict()
 
-        self.D = {'RPM': 700,
-             'MAF': 0,
-             'TMP': 0,
-             'OXY': 0,
-             'KMH': 0,
-             'BAT': 0,
-             'THL': 0,
-             'INJ': 0,
-             'TIM': 0,
-             'IDL': 0,
-             'AFS': 0,
-             'AFL': 0,
-             'DR0': 0,
-             'DR1': 0}
+        self.D = {'RPM': 0,
+                  'MAF': 0,
+                  'TMP': 0,
+                  'OXY': 0,
+                  'KMH': 0,
+                  'BAT': 0,
+                  'THL': 0,
+                  'INJ': 0,
+                  'TIM': 0,
+                  'IDL': 0,
+                  'AFS': 0,
+                  'AFL': 0,
+                  'DR0': 0,
+                  'DR1': 0}
 
         self.fileName = datetime.datetime.now().strftime("%d-%m-%y-%H-%M")
 
@@ -79,8 +75,8 @@ class ReadStream(threading.Thread):
                 self.port.write('\xFF\xFF\xEF')
                 time.sleep(2)
 
-
-        self.port.write('\x5A\x01\x5A\x05\x5A\x08\x5A\x09\x5A\x0B\x5A\x0C\x5A\x0D\x5A\x15\x5A\x16\x5A\x17\x5A\x1A\x5A\x1C\x5A\x1E\x5A\x1F\xF0')
+        self.port.write(
+            '\x5A\x01\x5A\x05\x5A\x08\x5A\x09\x5A\x0B\x5A\x0C\x5A\x0D\x5A\x15\x5A\x16\x5A\x17\x5A\x1A\x5A\x1C\x5A\x1E\x5A\x1F\xF0')
 
         ####### Sensors to read:
         ## [00] 0x01 RPM
@@ -104,7 +100,7 @@ class ReadStream(threading.Thread):
             Header = 255
             returnBytes = 14
             frameStart = self.port.read(3)
-            frameList = map(ord,frameStart)
+            frameList = map(ord, frameStart)
 
             if frameList[1] == Header and frameList[2] == returnBytes:
                 print ('Data stream aligned, streaming from ECU.')
@@ -117,23 +113,23 @@ class ReadStream(threading.Thread):
             if incomingData:
                 # We have a full line we could store into a file here
 
-                dataList = map(ord,incomingData)
-                #convertValues (dataList)
+                dataList = map(ord, incomingData)
+                # convertValues (dataList)
                 self.D['RPM'] = int(round((dataList[0] * 12.5), 2))
                 self.D['RPM'] = 500
 
             else:
                 pass
 
-    def convertValues (self, readvalues):
+    def convertValues(self, readvalues):
         self.integrity = False  # Until all registers have been processed, data is marked invalid
 
-        self.D['RPM'] = int(round((readvalues[0] * 12.5),2))
+        self.D['RPM'] = int(round((readvalues[0] * 12.5), 2))
         self.D['MAF'] = readvalues[1] * 5
         self.D['TMP'] = readvalues[2] - 50
         self.D['OXY'] = readvalues[3] * 10
-        self.D['KMH'] = int(round (readvalues[4] * 2))
-        self.D['BAT'] = round(((readvalues[5] * 80) / 1000),1)
+        self.D['KMH'] = int(round(readvalues[4] * 2))
+        self.D['BAT'] = round(((readvalues[5] * 80) / 1000), 1)
         self.D['THL'] = readvalues[6] * 20
         self.D['INJ'] = readvalues[7] / 100
         self.D['TIM'] = 110 - readvalues[8]
@@ -147,7 +143,7 @@ class ReadStream(threading.Thread):
         return True
 
     def getIntegrity(self):
-        return self.integrity;
+        return self.integrity
 
-    def getData (self):
-        return self.D;
+    def getData(self):
+        return self.D
