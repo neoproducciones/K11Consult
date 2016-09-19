48e7c716abee343db412ec792e28efddfd25da5d
Javier <neoproducciones@gmail.com>
2016-09-19 18:54:34 +0200

    Modificado paso de parámetros

--- streamLiveDataConsole.py
+++ streamLiveDataConsole.py
@@ -26,6 +26,8 @@ PORT = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
 D = dict()
 incomingData = serialThread.ReadStream(D, PORT, True)
 
+D = {'RPM': 0, 'MAF': 0, 'TMP': 0, 'OXY': 0, 'KMH': 0, 'BAT': 0, 'THL': 0, 'INJ': 0, 'TIM': 0, 'IDL': 0,
+     'AFS': 0, 'AFL': 0, 'DR0': 0, 'DR1': 0}
 
 while True:
 
