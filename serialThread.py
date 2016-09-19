098f8e343db443a8fdbd7e2845079c7d6ff379a6
Javier <neoproducciones@gmail.com>
2016-09-19 18:56:12 +0200

    Modificado paso de parámetros

--- serialThread.py
+++ serialThread.py
@@ -32,7 +32,7 @@ class ReadStream(threading.Thread):
         self.d = dict
         self.d = data
 
-        self.d = {'RPM': 300, 'MAF': 0, 'TMP': 0, 'OXY': 0, 'KMH': 0, 'BAT': 0, 'THL': 0, 'INJ': 0, 'TIM': 0, 'IDL': 0,
+        self.d = {'RPM': 0, 'MAF': 0, 'TMP': 0, 'OXY': 0, 'KMH': 0, 'BAT': 0, 'THL': 0, 'INJ': 0, 'TIM': 0, 'IDL': 0,
                   'AFS': 0, 'AFL': 0, 'DR0': 0, 'DR1': 0}
 
         self.fileName = datetime.datetime.now().strftime("%d-%m-%y-%H-%M")
