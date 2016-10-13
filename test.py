f827d4742302dd1ac1d965f7efca69640d74037c
Javier <neoproducciones@gmail.com>
2016-10-13 23:29:21 +0200

    -

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-print ("Hola")
+from time import sleep
 
 #  Encoder pinout:
 #  CLK - blue - pin #13
@@ -8,13 +8,35 @@ print ("Hola")
 #  + - white - pin #1
 #  GND - black - pin #9 (ground)
 
+clk = 13
+dt = 11
+sw = 12
+
 #  Usamos numeracion fisica de los pines
 GPIO.setmode(GPIO.BOARD)
 #  Configuramos cada uno de los pines
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+clkLastState = GPIO.input(clk)
+counter = 0
 
 while True:
-    if not GPIO.input(12):
+    if not GPIO.input(sw):
         print("Pulsado")
+    else:
+        print("")
+
+    clkState = GPIO.input(clk)
+    dtState = GPIO.input(dt)
+    if clkState != clkLastState:
+        if dtState != clkState:
+            counter += 1
+            print("Derecha")
+        else:
+            counter -= 1
+            print("Izquieda")
+        print counter
+    clkLastState = clkState
+    sleep(0.01)
