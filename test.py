e3ddb9b24ded9d9e8f8499512ae12ac2aa98c56a
Javier <neoproducciones@gmail.com>
2016-10-13 23:14:38 +0200

    -

--- test.py
+++ test.py
@@ -2,17 +2,21 @@ import RPi.GPIO as GPIO
 print ("Hola")
 
 #  Encoder pinout:
-#  CLK - blue
-#  DT - brown
-#  SW - gray - pin #11 (GPIO17 - GPIO_GEN0)
-#  + - white - pin
+#  CLK - blue - pin #13
+#  DT - brown - pin #11
+#  SW - gray - pin #12
+#  + - white - pin #1
 #  GND - black - pin #9 (ground)
 
 #  Usamos numeracion fisica de los pines
 GPIO.setmode(GPIO.BOARD)
+#  Configuramos cada uno de los pines
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
-    if GPIO.input(11):
-        print("Pin 11 estado ALTO")
+    if GPIO.input(12):
+        print("Pin 12 estado ALTO")
     else:
-        print("Pin 11 estado BAJO")
+        print("Pulsado boton")
