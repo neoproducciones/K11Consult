8608ea85a15179b0b71dc289468bcc10170de8d3
Javier <neoproducciones@gmail.com>
2016-10-14 23:58:20 +0200

    -

--- test-lcd.py
+++ test-lcd.py
@@ -5,7 +5,8 @@ rows = 4
 
 lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
 
-lcd.printline(0, 'LCM2004 IIC V2'.center(cols))
-lcd.printline(1, 'and'.center(cols))
-lcd.printline(2, 'python-')
-lcd.printline(3, 'liquidcrystal_i2c'.rjust(cols))
+i = 0
+
+while True:
+    lcd.printline(i, '>>')
+    i = i + 1
