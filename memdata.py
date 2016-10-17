d76f2486ed5fdb0b4427da5f09c4c7583c14990a
Javier <neoproducciones@gmail.com>
2016-10-17 18:48:59 +0200

    -

--- memdata.py
+++ memdata.py
@@ -16,7 +16,7 @@ def init():
     global vis_ms
 
     D = {'KMH': 0, 'ECO': 0, 'RPM': 0, 'TIM': 0, 'TMP': 0, 'OXY': 0, 'BAT': 0, 'INJ': 0,
-         'THL': 0, 'MAF': 0, 'IDL': 0, 'AFS': 0, 'AFL': 0, 'DR0': 0, 'DR1': 0}
+         'THL': 0, 'MAF': 0, 'IDL': 0, 'AFS': 0, 'AFL': 0, 'DR0': 0, 'DR1': 0, 'ERR': 0}
 
     integrity = True
 
--- visThread.py
+++ visThread.py
@@ -4,15 +4,13 @@
 
 import sys, time, math, threading, datetime
 import memdata
+import liquidcrystal_i2c
 
 
 class visThread(threading.Thread):
     def __init__(self):
 
-        self.sensors = list
-        # List of all sensors. Each sensor is 9 chars
-
-        self.sbuf = list
+        self.sbuf = ""
         # Screen lines buffer. Each line is 18 usable chars
 
         self.viewport = 0
@@ -23,62 +21,34 @@ class visThread(threading.Thread):
         self.cols = 20
         # Screen size
 
+        self.sensor_len  = 10
+        self.sensors_per_display = 8
+
+        self.lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=self.rows)
+        # We create an instance of the LCD screen driver
+
         threading.Thread.__init__(self)
         self.daemon = True
         self.start()
 
     def run(self):
         while memdata.vis:
-            update_screen(self)
-            console_send(screenlines)
-            lcd_send(screenlines)
-            sleep(memdata.vis_ms)
+            self.write_scr_buffer()
+            self.lcd_send()
+            time.sleep(memdata.vis_ms)
 
-    def populate_sensors_list(self):
-        for sensor in memdata.D:
-            self.sensors.append("cosas")
-        self.sensors.append ("KMH:" + str(memdata.D['KMH']).rjust(5))
-        self.sensors.append ("ECO:" + str(7.4).rjust(5))  #  Fake as ***
+    def write_scr_buffer(self):
+        # We create the whole screen buffer as one line
+        self.sbuf=""
+        for k,v in memdata.D:
+            self.sbuf += (" " + k + ":" + v.rjust(5))
+        # Here and on we can write the engine error codes.
+        # We must assure the screen buffer lenght is a multiple of rows*col
 
-
-        return True
-
-    def populate_screen_buffer(self):
-        return True
-
-
-    def update_screen(self):
-
-
-        -  Método: Inserta valores en lista de sensores "XXX NNNNN"
-        -  Método: Convierte lista de sensores en líneas
-
-        Primera pantalla
-
-        1-Velocidad  2-Consumo instantáneo
-        3-RPM        4-Avance encendido
-        5-Temperat.  6-Oxígeno
-        7-Batería    8-ms Inyectores
-
-        Segunda pantalla
-
-
-
-       - memdata.D['RPM'] = int(round((readvalues[0] * 12.5), 0))
-       - memdata.D['MAF'] = readvalues[1] * 5
-       - memdata.D['TMP'] = readvalues[2] - 50
-       - memdata.D['OXY'] = readvalues[3] * 10
-       - memdata.D['KMH'] = int(round(readvalues[4] * 2))
-       - memdata.D['BAT'] = round(((readvalues[5] * 80) / 1000), 1)
-        memdata.D['THL'] = readvalues[6] * 20
-        memdata.D['INJ'] = readvalues[7] / 100
-       - memdata.D['TIM'] = 110 - readvalues[8]
-        memdata.D['IDL'] = readvalues[9] / 2
-        memdata.D['AFS'] = readvalues[10]
-        memdata.D['AFL'] = readvalues[11]
-        memdata.D['DR0'] = readvalues[12]
-        memdata.D['DR1'] = readvalues[13]
-
-        memdata.integrity = True
-        return True
-
+    def lcd_send(self):
+        start = self.viewport * self.sensors_per_display * self.sensor_len
+        for i in range(4):
+            end = start + self.cols
+            linea = self.sbuf[start:end]
+            self.lcd.printline (i, linea)
+            start += self.cols
