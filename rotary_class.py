dce664be9f335c095f554b2ed67116f5a92d628e
Javier <neoproducciones@gmail.com>
2016-10-22 12:50:33 +0200

    -

--- rotary_class.py
+++ rotary_class.py
@@ -31,6 +31,7 @@ class RotaryEncoder:
         self.pinB = pinB
         self.button = button
         self.callback = callback
+        self.primero = True # Adding this because it's a two changes per detent encoder
 
         GPIO.setmode(GPIO.BOARD) # Changed from the original GPIO.BCM for easier naming
 
@@ -49,44 +50,20 @@ class RotaryEncoder:
 
         # Add event detection to the GPIO inputs
         # Tuning different bouncetime values to avoid false readings
-        GPIO.add_event_detect(self.pinA, GPIO.FALLING, callback=self.switch_event, bouncetime=10)
-        GPIO.add_event_detect(self.pinB, GPIO.FALLING, callback=self.switch_event, bouncetime=10)
+        GPIO.add_event_detect(self.pinA, GPIO.FALLING, callback=self.switch_event_A, bouncetime=100)
+        # GPIO.add_event_detect(self.pinB, GPIO.FALLING, callback=self.switch_event_B, bouncetime=10)
         GPIO.add_event_detect(self.button, GPIO.BOTH, callback=self.button_event, bouncetime=50)
         return
 
     # Call back routine called by switch events
-    def switch_event(self, switch):
-        if GPIO.input(self.pinA):
-            self.rotary_a = 1
-        else:
-            self.rotary_a = 0
-
+    def switch_event_A(self, switch):
         if GPIO.input(self.pinB):
-            self.rotary_b = 1
+            event=self.CLOCKWISE
         else:
-            self.rotary_b = 0
+            event=self.ANTICLOCKWISE
 
-        self.rotary_c = self.rotary_a ^ self.rotary_b
-        new_state = self.rotary_a * 4 + self.rotary_b * 2 + self.rotary_c * 1
-        delta = (new_state - self.last_state) % 4
-        self.last_state = new_state
-        event = 0
+        self.callback(event)
 
-        if delta == 1:
-            if self.direction == self.CLOCKWISE:
-                # print "Clockwise"
-                event = self.direction
-            else:
-                self.direction = self.CLOCKWISE
-        elif delta == 3:
-            if self.direction == self.ANTICLOCKWISE:
-                # print "Anticlockwise"
-                event = self.direction
-            else:
-                self.direction = self.ANTICLOCKWISE
-        if event > 0:
-            self.callback(event)
-        return
 
     # Push button up event
     def button_event(self, button):
